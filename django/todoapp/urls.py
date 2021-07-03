78618fd438be03a46b82b0fe60fd808f66905bfd
ootsubo1192 <ootubo_stock@yahoo.co.jp>
2021-07-04 05:32:52 +0900

    add todoapp

---
+++ django/todoapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import TodoList, TodoDetail
+
+urlpatterns = [
+    path('list/', TodoList.as_view()),
+    path('detail/<int:pk>/', TodoDetail.as_view())
+]
+
