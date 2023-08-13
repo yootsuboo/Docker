cc250faa2b0296e24a9a756cac129c050238b548
ootsubo1192 <ootubo_stock@yahoo.co.jp>
2023-08-13 12:23:09 +0900

    modified docker build file

--- go_dev/run.sh
+++ go_dev/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-docker-compose build
-docker-compose up -d
+echo "コンテナイメージの作成"
+docker-compose build | echo $?
+
+echo "コンテナのバックグラウンドでの実行"
+docker-compose up -d | echo $?
+
+echo "コンテナに接続"
 docker-compose exec boiler-plate ash
+
+exit 0
